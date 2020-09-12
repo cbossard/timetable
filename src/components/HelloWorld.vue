9fcf882f9a02d5c19886302e98affb40e583728b
Cécilia Bossard <bossard@codelutin.com>
2020-09-12 11:28:20 +0200

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,199 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-stepper v-model="e6" vertical>
+      <v-stepper-step :complete="e6 > 1" step="1">
+        Select a file
+        <small>Import a CSV file containing all your sessions</small>
+      </v-stepper-step>
+  
+      <v-stepper-content step="1">
+        <v-card color="grey lighten-4" class="mb-12" height="70px">
+            <simple-csv-parser :columns="columns" @on-load="onLoad" @on-error="onError"></simple-csv-parser>
+        </v-card>
+        <v-btn color="primary" @click="e6 = 2">Continue</v-btn>
+      </v-stepper-content>
+  
+      <v-stepper-step :complete="e6 > 2" step="2">Sign-in to your Google account</v-stepper-step>
+  
+      <v-stepper-content step="2">
+      
+          <v-banner v-if='!authorized'>
+            <v-avatar
+              slot="icon"
+              color="primary"
+              size="40"
+            >
+              <v-icon
+                icon="mdi-lock"
+                color="white"
+              >
+                mdi-lock
+              </v-icon>
+            </v-avatar>
+
+            Please log in to your Google account
+
+            <template v-slot:actions>
+              <v-btn text color="error" @click="handleAuthClick">Sign In</v-btn>
+            </template>
+          </v-banner>
+
+          <v-banner v-if='authorized'>
+            <v-avatar
+              slot="icon"
+              color="primary"
+              size="40"
+            >
+              <v-icon
+                icon="mdi-lock"
+                color="white"
+              >
+                mdi-lock
+              </v-icon>
+            </v-avatar>
+
+            Your are already signed-in
+
+            <template v-slot:actions>
+              <v-btn text color="error" @click="handleSignoutClick">Sign Out</v-btn>
+            </template>
+          </v-banner>         
+      
+        <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
+        <v-btn text @click="e6 = 1">Cancel</v-btn>
+      </v-stepper-content>
+  
+      <v-stepper-step :complete="e6 > 3" step="3">Create events</v-stepper-step>
+  
+      <v-stepper-content step="3">
+        <v-banner>Events will be created in your 'primary' calendar</v-banner>
+       
+        <v-btn color="primary" @click="createEvent">Continue</v-btn>
+        <v-btn text @click="e6 = 2">Cancel</v-btn>
+      </v-stepper-content>
+  
+      <v-stepper-step step="4">Done !</v-stepper-step>
+      <v-stepper-content step="4">
+        <v-banner>Events have been created ! Check your Google Calendar :-)</v-banner>
+      </v-stepper-content>
+    </v-stepper>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  import { SimpleCsvParser } from 'vue-simple-csv-parser';
+
+  export default {
+    name: 'HelloWorld',
+    
+    components: {
+      SimpleCsvParser,
+    },
+
+    data: () => ({
+      e6: 1,
+      columns: [
+          { name: 'Date', value: 'Date' },
+          { name: 'DayOfweek', value: 'dayOfWeek' },
+          { name: 'Period', value: 'period' },
+          { name: 'StartTime', value: 'startTime' },
+          { name: 'EndTime', value: 'endTime' },
+          { name: 'Session', value: 'session', isOptional: true },
+          { name: 'Emails', value: 'emails', isOptional: true },
+          { name: 'Location', value: 'location', isOptional: true },
+        ],
+        results: null,
+
+        calendarName: "",
+        authorized: false,
+        api: undefined,
+    }),
+
+
+    created() {
+      if (this.$isAuthenticated()) {
+        this.authorized = true;
+      }
+    },
+
+    methods: {
+      onLoad(results) {
+        this.results = results;
+      },
+      onError(message) {
+        // handle error;
+        console.log(message)
+      },
+
+      /**
+       *  Sign in the user upon button click.
+       */
+      handleAuthClick() {
+         this.$login()
+          .then(() => {
+            this.authorized = true;
+          });
+      },      
+
+       /**
+       *  Sign out the user upon button click.
+       */
+      handleSignoutClick() {
+        this.$logout()
+          .then(() => {
+            this.authorized = false;
+          });
+      },
+
+      createEvent() {
+
+        this.$getGapiClient().then((gapi) => {
+        
+          var batch = gapi.client.newBatch();
+        
+          this.results.forEach((event) => {
+            if(event.Session) {
+              var startDate = this.$moment(event.Date + " " + event.StartTime, "DD/MM/YY hh:mm:ss");
+              var endDate = this.$moment(event.Date + " " + event.EndTime, "DD/MM/YY hh:mm:ss");             
+
+              var newEvent = {
+                'summary': event.Session,
+                'description': event.Session,
+                'start': {
+                  'dateTime': startDate.toDate()
+                },
+                'end': {
+                  'dateTime': endDate.toDate()
+                },
+              }; 
+
+              if(event.Location) {
+                newEvent.location = event.Location;
+              }
+
+              if(event.GroupEmail) {
+                newEvent.attendees = [
+                  {'email': event.GroupEmail}
+                ]
+              }
+
+              batch.add(gapi.client.calendar.events.insert({
+                  'calendarId': 'primary',
+                  'resource': newEvent
+              }));
+            }
+            
+          }); 
+
+          batch.then(function(){
+            console.log('all jobs done!!!');
+            
+          });
+
+
+        }).then(() => {this.e6 = 4;});
+      },
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+// import VueGoogleApi from 'vue-google-api';
+import VueGAPI from 'vue-gapi';
 
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: 'AIzaSyA3u8gveFQVh9qj3c66L9_fyO15ymQeuvg',
+  clientId: '919241210274-nnc284pu108au36e9qe80hjlbimnclpg.apps.googleusercontent.com',
+  scope: 'https://www.googleapis.com/auth/calendar.events',
+  discoveryDocs: [ 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest' ]
+}
+
+Vue.use(VueGAPI, config)
+Vue.use(require('vue-moment'));
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
