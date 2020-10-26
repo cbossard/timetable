44f55681d7d39287cb1a10a7f0f65580a5a185c4
Cécilia Bossard <bossard@codelutin.com>
2020-10-26 16:05:32 +0100

    Date parsing problem

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,197 +5,190 @@
         Select a file
         <small>Import a CSV file containing all your sessions</small>
       </v-stepper-step>
-  
+
       <v-stepper-content step="1">
         <v-card color="grey lighten-4" class="mb-12" height="70px">
-            <simple-csv-parser :columns="columns" @on-load="onLoad" @on-error="onError"></simple-csv-parser>
+          <simple-csv-parser
+            :columns="columns"
+            @on-load="onLoad"
+            @on-error="onError"
+          ></simple-csv-parser>
         </v-card>
         <v-btn color="primary" @click="e6 = 2">Continue</v-btn>
       </v-stepper-content>
-  
-      <v-stepper-step :complete="e6 > 2" step="2">Sign-in to your Google account</v-stepper-step>
-  
+
+      <v-stepper-step :complete="e6 > 2" step="2"
+        >Sign-in to your Google account</v-stepper-step
+      >
+
       <v-stepper-content step="2">
-      
-          <v-banner v-if='!authorized'>
-            <v-avatar
-              slot="icon"
-              color="primary"
-              size="40"
+        <v-banner v-if="!authorized">
+          <v-avatar slot="icon" color="primary" size="40">
+            <v-icon icon="mdi-lock" color="white"> mdi-lock </v-icon>
+          </v-avatar>
+
+          Please log in to your Google account
+
+          <template v-slot:actions>
+            <v-btn text color="error" @click="handleAuthClick">Sign In</v-btn>
+          </template>
+        </v-banner>
+
+        <v-banner v-if="authorized">
+          <v-avatar slot="icon" color="primary" size="40">
+            <v-icon icon="mdi-lock" color="white"> mdi-lock </v-icon>
+          </v-avatar>
+
+          Your are already signed-in
+
+          <template v-slot:actions>
+            <v-btn text color="error" @click="handleSignoutClick"
+              >Sign Out</v-btn
             >
-              <v-icon
-                icon="mdi-lock"
-                color="white"
-              >
-                mdi-lock
-              </v-icon>
-            </v-avatar>
+          </template>
+        </v-banner>
 
-            Please log in to your Google account
-
-            <template v-slot:actions>
-              <v-btn text color="error" @click="handleAuthClick">Sign In</v-btn>
-            </template>
-          </v-banner>
-
-          <v-banner v-if='authorized'>
-            <v-avatar
-              slot="icon"
-              color="primary"
-              size="40"
-            >
-              <v-icon
-                icon="mdi-lock"
-                color="white"
-              >
-                mdi-lock
-              </v-icon>
-            </v-avatar>
-
-            Your are already signed-in
-
-            <template v-slot:actions>
-              <v-btn text color="error" @click="handleSignoutClick">Sign Out</v-btn>
-            </template>
-          </v-banner>         
-      
         <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
         <v-btn text @click="e6 = 1">Cancel</v-btn>
       </v-stepper-content>
-  
+
       <v-stepper-step :complete="e6 > 3" step="3">Create events</v-stepper-step>
-  
+
       <v-stepper-content step="3">
         <v-banner>Events will be created in your 'primary' calendar</v-banner>
-       
+
         <v-btn color="primary" @click="createEvent">Continue</v-btn>
         <v-btn text @click="e6 = 2">Cancel</v-btn>
       </v-stepper-content>
-  
+
       <v-stepper-step step="4">Done !</v-stepper-step>
       <v-stepper-content step="4">
-        <v-banner>Events have been created ! Check your Google Calendar :-)</v-banner>
+        <v-banner
+          >Events have been created ! Check your Google Calendar :-)</v-banner
+        >
       </v-stepper-content>
     </v-stepper>
   </v-container>
 </template>
 
 <script>
+import { SimpleCsvParser } from "vue-simple-csv-parser";
 
-  import { SimpleCsvParser } from 'vue-simple-csv-parser';
+export default {
+  name: "HelloWorld",
 
-  export default {
-    name: 'HelloWorld',
-    
-    components: {
-      SimpleCsvParser,
+  components: {
+    SimpleCsvParser,
+  },
+
+  data: () => ({
+    e6: 1,
+    columns: [
+      { name: "Holyday", value: "Holyday", isOptional: true },
+      { name: "Date", value: "Date" },
+      { name: "DayOfweek", value: "dayOfWeek" },
+      { name: "Period", value: "period" },
+      { name: "StartTime", value: "startTime" },
+      { name: "EndTime", value: "endTime" },
+      { name: "Type", value: "type" },
+      { name: "Session", value: "session", isOptional: true },
+      { name: "Emails", value: "emails", isOptional: true },
+      { name: "Location", value: "location", isOptional: true },
+      { name: "StartDateTime", value: "StartDateTime" },
+      { name: "EndDateTime", value: "EndDateTime" },
+    ],
+    results: null,
+
+    calendarName: "",
+    authorized: false,
+    api: undefined,
+  }),
+
+  created() {
+    if (this.$isAuthenticated()) {
+      this.authorized = true;
+    }
+  },
+
+  methods: {
+    onLoad(results) {
+      this.results = results;
+    },
+    onError(message) {
+      // handle error;
+      console.log(message);
     },
 
-    data: () => ({
-      e6: 1,
-      columns: [
-          { name: 'Holyday', value: 'Holyday', isOptional: true },
-          { name: 'Date', value: 'Date' },
-          { name: 'DayOfweek', value: 'dayOfWeek' },
-          { name: 'Period', value: 'period' },
-          { name: 'StartTime', value: 'startTime' },
-          { name: 'EndTime', value: 'endTime' },
-          { name: 'Type', value: 'type' },
-          { name: 'Session', value: 'session', isOptional: true },
-          { name: 'Emails', value: 'emails', isOptional: true },
-          { name: 'Location', value: 'location', isOptional: true },
-        ],
-        results: null,
-
-        calendarName: "",
-        authorized: false,
-        api: undefined,
-    }),
-
-
-    created() {
-      if (this.$isAuthenticated()) {
+    /**
+     *  Sign in the user upon button click.
+     */
+    handleAuthClick() {
+      this.$login().then(() => {
         this.authorized = true;
-      }
+      });
     },
 
-    methods: {
-      onLoad(results) {
-        this.results = results;
-      },
-      onError(message) {
-        // handle error;
-        console.log(message)
-      },
+    /**
+     *  Sign out the user upon button click.
+     */
+    handleSignoutClick() {
+      this.$logout().then(() => {
+        this.authorized = false;
+      });
+    },
 
-      /**
-       *  Sign in the user upon button click.
-       */
-      handleAuthClick() {
-         this.$login()
-          .then(() => {
-            this.authorized = true;
-          });
-      },      
-
-       /**
-       *  Sign out the user upon button click.
-       */
-      handleSignoutClick() {
-        this.$logout()
-          .then(() => {
-            this.authorized = false;
-          });
-      },
-
-      createEvent() {
-
-        this.$getGapiClient().then((gapi) => {
-        
+    createEvent() {
+      this.$getGapiClient()
+        .then((gapi) => {
           var batch = gapi.client.newBatch();
-        
+
           this.results.forEach((event) => {
-            if(event.Session && !event.Hollyday) {
-              var startDate = this.$moment(event.Date + " " + event.StartTime, "DD/MM/YY hh:mm:ss");
-              var endDate = this.$moment(event.Date + " " + event.EndTime, "DD/MM/YY hh:mm:ss");             
+            if (event.Session && !event.Hollyday) {
+              var startDate = this.$moment(
+                event.StartDateTime + " " + event.StartTime,
+                "DD/MM/YY hh:mm:ss"
+              );
+              var endDate = this.$moment(
+                event.StartDateTime + " " + event.EndTime,
+                "DD/MM/YY hh:mm:ss"
+              );
 
               var newEvent = {
-                'summary': event.Session,
-                'description': event.Session,
-                'start': {
-                  'dateTime': startDate.toDate()
+                summary: event.Session,
+                description: event.Session,
+                start: {
+                  dateTime: startDate.toDate(),
                 },
-                'end': {
-                  'dateTime': endDate.toDate()
+                end: {
+                  dateTime: endDate.toDate(),
                 },
-              }; 
+              };
 
-              if(event.Location) {
+              if (event.Location) {
                 newEvent.location = event.Location;
               }
 
-              if(event.GroupEmail) {
-                newEvent.attendees = [
-                  {'email': event.GroupEmail}
-                ]
+              if (event.GroupEmail) {
+                newEvent.attendees = [{ email: event.GroupEmail }];
               }
 
-              batch.add(gapi.client.calendar.events.insert({
-                  'calendarId': 'primary',
-                  'resource': newEvent
-              }));
+              batch.add(
+                gapi.client.calendar.events.insert({
+                  calendarId: "primary",
+                  resource: newEvent,
+                })
+              );
             }
-            
-          }); 
-
-          batch.then(function(){
-            console.log('all jobs done!!!');
-            
           });
 
-
-        }).then(() => {this.e6 = 4;});
-      },
-    }
-  }
+          batch.then(function () {
+            console.log("all jobs done!!!");
+          });
+        })
+        .then(() => {
+          this.e6 = 4;
+        });
+    },
+  },
+};
 </script>
